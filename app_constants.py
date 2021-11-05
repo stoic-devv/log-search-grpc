d19a5e5c6115872dedd837c242d3cb2184c61155
aditya <adityakanthale@gmail.com>
2021-11-04 21:39:51 -0500

    adding app constants

---
+++ app_constants.py
@@ -0,0 +1,9 @@
+from enum import Enum
+
+class Constants(Enum):
+    URL = "https://uvz1k07zeg.execute-api.us-east-2.amazonaws.com/prod/logs"
+    TIME_PARAM = 't'
+    INTERVAL_PARAM = 'dt'
+    
+    HOST_NAME = 'localhost:'
+    PORT_NUM = '50051'
