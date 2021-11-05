3d9a1432d0d50ef918e628d1979bb3a2e3a3cb17
aditya <adityakanthale@gmail.com>
2021-11-04 21:40:46 -0500

    incorporate constants

--- log_search_client.py
+++ log_search_client.py
@@ -1,16 +1,24 @@
 import logging
+from app_constants import Constants
 import log_search_pb2_grpc
 import log_search_pb2
 import grpc
 import sys
 
+"""
+Returns the params in proto format
+"""
 def get_params(args):
     if len(args) != 2:
         logging.error("Invalid arguments")
     return log_search_pb2.QueryParams(time_stamp=args[0], time_interval=args[1])
 
+"""
+Client code
+"""
 def run(args):
-    with grpc.insecure_channel('localhost:50051') as channel:
+    with grpc.insecure_channel(Constants.HOST_NAME.value+Constants.PORT_NUM.value) as channel:
+        logging.info('Sending message to server')
         stub = log_search_pb2_grpc.LogSearchStub(channel)
         response = stub.GetLogs(get_params(args))
         print(response.status_code, response.content)
--- log_search_server.py
+++ log_search_server.py
@@ -1,4 +1,6 @@
 from concurrent import futures
+from app_constants import Constants
+
 import logging
 import requests
 import grpc
@@ -8,19 +10,24 @@ import log_search_pb2
 
 class LogSearchServicer(log_search_pb2_grpc.LogSearchServicer):
 
+    """
+        Returns the log messages from Lambda via a GET request
+    """
     def GetLogs(self, request, context):
-        url = "https://uvz1k07zeg.execute-api.us-east-2.amazonaws.com/prod/logs"
-        params = {'t': request.time_stamp, 'dt': request.time_interval}
-
-        response = requests.get(url=url, params=params)
+        logging.info("Received client message")
+        params = {Constants.TIME_PARAM.value: request.time_stamp, Constants.INTERVAL_PARAM.value: request.time_interval}
+        response = requests.get(url=Constants.URL.value, params=params)
         return log_search_pb2.Response(status_code=response.status_code, content=response.content)
         
-
+"""
+Runs the server listening to the client grpc calls
+"""
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     log_search_pb2_grpc.add_LogSearchServicer_to_server(
         LogSearchServicer(), server)
-    server.add_insecure_port('[::]:50051')
+    server.add_insecure_port('[::]:'+ Constants.PORT_NUM.value)
+    logging.info('Starting server')
     server.start()
     server.wait_for_termination()
 
